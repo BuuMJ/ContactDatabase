50e300621cec75eb30f84577f02675ea0fb845a0
BuuMJ <nxt03091999@gmail.com>
2023-10-29 10:04:55 +0700

    desgin UI

---
+++ app/src/main/java/com/example/contactdatabase/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.contactdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
